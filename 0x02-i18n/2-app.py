c44d30a5803cf94c523ff04316bad61f1d28873b
Mellanie <mellanie.achieng@gmail.com>
2023-03-29 21:09:06 +0300

    code style fix

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,7 +4,8 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-#@babel.localeselector
+
+# @babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
